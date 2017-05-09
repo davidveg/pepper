e2217a58385b0c012b3d938ed7f84b81fbbb73d4
davidvegabr <davidveg@gmail.com>
2017-05-09 12:09:14 -0300

    adding firebase facebook auth and email/pass auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,122 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  cuisines : FirebaseListObservable<any[]>;
+  restaurants : Observable<any[]>;
+  name;
+  displayName;
+  photoURL;
+  logged = false;
+
+  constructor(private af : AngularFire, private http: Http) {
+  }
+
+  ngOnInit() {
+    this.af.auth.subscribe(authState => {
+      if(!authState){
+        this.displayName = null;
+        this.photoURL = null;
+        this.logged = false;
+        return;
+      }
+        let userRef = this.af.database.object('/users/' + authState.uid);
+        userRef.subscribe(user => {
+          let url = `https://graph.facebook.com/v2.8/${authState.facebook.uid}?fields=first_name,last_name&access_token=${user.accessToken}`;
+          this.http.get(url).subscribe(response => {
+            let user = response.json();
+            userRef.update({
+              firstName: user.first_name,
+              lastName : user.last_name
+            });
+          })
+        })
+
+        this.displayName = authState.auth.displayName;
+        this.photoURL = authState.auth.photoURL;
+        console.log("LOGGED IN", authState);
+        this.logged = true;
+    });
+    // this.af.auth.subscribe(authStateEmail => {
+
+    // });
+
+    this.cuisines = this.af.database.list("/cuisines");
+    this.restaurants = this.af.database.list("/restaurants")
+      .map( restaurants => {
+        console.log("BEFORE MAP", restaurants);
+        restaurants.map(restaurant => {
+          restaurant.cuisineType = this.af.database.object('/cuisines/' + restaurant.cuisine);
+          console.log("AFTER MAP", restaurants);
+        })
+        return restaurants;
+      });
+  }
+
+  login() {
+    this.af.auth.login({
+      provider : AuthProviders.Facebook,
+      method : AuthMethods.Popup,
+      scope : ['public_profile', 'user_friends', 'user_likes']
+    }).then( (authState: any) => { 
+      this.af.database.object('/users/' + authState.uid).update({
+        accessToken : authState.facebook.accessToken
+      });
+      console.log("AFTER LOGIN", authState);
+    });
+  }
+
+  logout() {
+    this.af.auth.logout();
+  }
+
+  register() {
+    this.af.auth.createUser({
+      email : "testemailauth@gmail.com",
+      password : "125692@"
+    }).then(authStateEmail => {
+      authStateEmail => console.log("REGISTER-THEN ", authStateEmail)
+    }).catch(error => console.log("Error: ", error))
+  }
+
+  add(param) {
+    this.cuisines.push({
+      name: param.value
+    });
+  }
+
+  update(param) {
+    this.af.database.object("/restaurant")
+      .update({
+        name : param.value,
+        followers : 50,
+        address : { city: "New York",
+                    country: "US",
+                    district: "Blob",
+                    state: "NY",
+                    street: "Major Street",
+                    followers: 50,
+                    name:  "Mobs",
+                    rate: 9.1,
+                    type: "Italian"
+                  }
+      });
+  }
+
+  remove() {
+    this.af.database.object("/restaurant")
+      .remove()
+      .then(x => { console.log("SUCCESS")})
+      .catch(error => console.log("ERROR", error));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+
+export const fireBaseConfig = {
+    apiKey: "AIzaSyBJwtCRp8_2SsEyNzr-PZG0A559DIP4JkY",
+    authDomain: "pepper-af4b0.firebaseapp.com",
+    databaseURL: "https://pepper-af4b0.firebaseio.com",
+    projectId: "pepper-af4b0",
+    storageBucket: "pepper-af4b0.appspot.com",
+    messagingSenderId: "457374243586"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(fireBaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
